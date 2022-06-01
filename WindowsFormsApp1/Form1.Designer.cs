3d7a96f17389c2ec2c7164cde940724e9f563473
bangkit <bangkit.ilhamz@gmail.com>
2022-06-01 17:16:58 +0700

    init

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox_salt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_valuetext = new System.Windows.Forms.TextBox();
+            this.button_encrypt = new System.Windows.Forms.Button();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_key = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.button_decrypt = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_passwordhash = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_result, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 64.88889F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35.11111F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 8.690176F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 91.30982F));
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.textBox_salt, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.textBox_key, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 1, 4);
+            this.tableLayoutPanel2.Controls.Add(this.textBox_valuetext, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.textBox_passwordhash, 1, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.38461F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.38461F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.38461F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30.76923F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 23.07692F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(794, 285);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // textBox_salt
+            // 
+            this.textBox_salt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_salt.Location = new System.Drawing.Point(71, 3);
+            this.textBox_salt.Multiline = true;
+            this.textBox_salt.Name = "textBox_salt";
+            this.textBox_salt.Size = new System.Drawing.Size(720, 37);
+            this.textBox_salt.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Salt";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password Hash";
+            // 
+            // textBox_valuetext
+            // 
+            this.textBox_valuetext.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_valuetext.Location = new System.Drawing.Point(71, 132);
+            this.textBox_valuetext.Multiline = true;
+            this.textBox_valuetext.Name = "textBox_valuetext";
+            this.textBox_valuetext.Size = new System.Drawing.Size(720, 81);
+            this.textBox_valuetext.TabIndex = 3;
+            // 
+            // button_encrypt
+            // 
+            this.button_encrypt.Location = new System.Drawing.Point(3, 3);
+            this.button_encrypt.Name = "button_encrypt";
+            this.button_encrypt.Size = new System.Drawing.Size(86, 23);
+            this.button_encrypt.TabIndex = 4;
+            this.button_encrypt.Text = "Encrypt";
+            this.button_encrypt.UseVisualStyleBackColor = true;
+            this.button_encrypt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_result.Location = new System.Drawing.Point(3, 294);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.Size = new System.Drawing.Size(794, 153);
+            this.textBox_result.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Vi Key";
+            // 
+            // textBox_key
+            // 
+            this.textBox_key.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_key.Location = new System.Drawing.Point(71, 46);
+            this.textBox_key.Multiline = true;
+            this.textBox_key.Name = "textBox_key";
+            this.textBox_key.Size = new System.Drawing.Size(720, 37);
+            this.textBox_key.TabIndex = 6;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.02778F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85.97222F));
+            this.tableLayoutPanel3.Controls.Add(this.button_encrypt, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.button_decrypt, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(71, 219);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(720, 63);
+            this.tableLayoutPanel3.TabIndex = 7;
+            // 
+            // button_decrypt
+            // 
+            this.button_decrypt.Location = new System.Drawing.Point(103, 3);
+            this.button_decrypt.Name = "button_decrypt";
+            this.button_decrypt.Size = new System.Drawing.Size(75, 23);
+            this.button_decrypt.TabIndex = 5;
+            this.button_decrypt.Text = "Decrypt";
+            this.button_decrypt.UseVisualStyleBackColor = true;
+            this.button_decrypt.Click += new System.EventHandler(this.button_decrypt_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Value Text";
+            // 
+            // textBox_passwordhash
+            // 
+            this.textBox_passwordhash.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_passwordhash.Location = new System.Drawing.Point(71, 89);
+            this.textBox_passwordhash.Multiline = true;
+            this.textBox_passwordhash.Name = "textBox_passwordhash";
+            this.textBox_passwordhash.Size = new System.Drawing.Size(720, 37);
+            this.textBox_passwordhash.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox textBox_valuetext;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_salt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_encrypt;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_key;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button button_decrypt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_passwordhash;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			string convert = Encrypt(textBox_salt.Text, textBox_key.Text, textBox_passwordhash.Text,textBox_valuetext.Text);
+			textBox_result.Text = convert;
+
+		}
+
+		private void button_decrypt_Click(object sender, EventArgs e)
+		{
+			string convert = Decrypt(textBox_salt.Text, textBox_key.Text, textBox_passwordhash.Text, textBox_valuetext.Text);
+			textBox_result.Text = convert;
+		}
+
+		private static string Encrypt(string saltString, string keyString, string passwordhash, string valueString)
+		{
+			byte[] plainTextBytes = Encoding.UTF8.GetBytes(valueString);
+
+			byte[] keyBytes = new Rfc2898DeriveBytes(passwordhash, Encoding.ASCII.GetBytes(saltString)).GetBytes(256 / 8);
+			var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.Zeros };
+			var encryptor = symmetricKey.CreateEncryptor(keyBytes, Encoding.ASCII.GetBytes(keyString));
+
+			byte[] cipherTextBytes;
+
+			using (var memoryStream = new MemoryStream())
+			{
+				using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+				{
+					cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+					cryptoStream.FlushFinalBlock();
+					cipherTextBytes = memoryStream.ToArray();
+					cryptoStream.Close();
+				}
+				memoryStream.Close();
+			}
+			return Convert.ToBase64String(cipherTextBytes);
+		}
+
+		public static string Decrypt(string saltString, string keyString, string passwordhash, string valueString)
+		{
+			byte[] cipherTextBytes = Convert.FromBase64String(valueString);
+			byte[] keyBytes = new Rfc2898DeriveBytes(passwordhash, Encoding.ASCII.GetBytes(saltString)).GetBytes(256 / 8);
+			var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.None };
+
+			var decryptor = symmetricKey.CreateDecryptor(keyBytes, Encoding.ASCII.GetBytes(keyString));
+			var memoryStream = new MemoryStream(cipherTextBytes);
+			var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+			byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+			int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+			memoryStream.Close();
+			cryptoStream.Close();
+			return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount).TrimEnd("\0".ToCharArray());
+		}
+
+        
+    }
+}
